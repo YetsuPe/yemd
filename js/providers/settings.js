63cece5e40538d8d90b47bbe59075431840a3b91
skeiter9 <skeiter97@gmail.com>
2014-08-20 22:54:46 -0500

    initialized provider yemd

---
+++ js/providers/settings.js
@@ -0,0 +1,19 @@
+function $settingsProvider(){
+	this.global=  {
+		fontSize       : '16px',
+		colorPrimary   : 'blue',
+		colorSecondary : 'red'
+	};  
+
+  this.folderIcons= 'icons/'; //default
+
+  this.$get=$get;
+
+  $get.$inject = ['$rootScope'];
+  function $get($rootScope){
+  	
+  }
+
+};
+angular.module('yemd')
+	.provider('$settings', $settingsProvider);
