8964fe1725f14cfedbb8361e07e2fa931d2f12b1
skeiter9 <skeiter97@gmail.com>
2014-12-07 22:18:18 -0500

    canvas component

---
+++ app/js/directives/canvas.js
@@ -0,0 +1,37 @@
+
+angular.module('yemd')
+	.directive('canvas', canvas);
+
+	function canvas($rootScope, $verge, $yemd, $window){
+		return {
+			scope:{
+				name: '@'
+			},
+			restrict: 'C',
+			controller: function($scope, $element, $attrs, $rootScope, $verge, $yemd, $window){
+				
+				$element.attr('class','canvas--default');
+				$scope.className = 'default';
+				
+				$rootScope.$on('changeTypeCanvas', function(e, name, className){ 
+	      	$scope.className = className;
+	      	resizeWindow();
+				});
+
+				function resizeWindow() {
+					if ( $verge.viewportW() >= $yemd.mqMedium && $scope.className !== 'default' ) {
+						$rootScope.$emit('changeTypeToolbar','appbar', '3rows');
+						$element.attr( 'class', 'canvas--'+ $scope.className  );
+					} else if( $verge.viewportW() < $yemd.mqMedium && $scope.className !== 'default' ) {
+						$rootScope.$emit('changeTypeToolbar','appbar', 'default');
+						$element.attr( 'class', 'canvas--default' );
+					};
+				}
+
+				$window.onresize = function(event) {
+					resizeWindow();
+				};
+
+			}
+		}
+	}
