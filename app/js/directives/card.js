57086ca00101f56404f4e0bc417999fa05988f5e
Skeiter9 <Skeiter97@gmail.com>
2015-01-04 15:13:23 -0500

    all cards

--- app/js/directives/card.js
+++ app/js/directives/card.js
@@ -5,20 +5,21 @@ angular.module('yemd')
 function card($rootScope, $yemd, checkWebpage){
 	var componentName = 'card';
 	return {
-		scope: {
-
-		},
+		scope: {},
 		controller: function($element, $attrs, $yemd, checkWebpage){
 
-			var type = ($attrs.card==='')?'':'--'+$attrs.card;
-			$element.addClass('card'+type);
+			var card = this;
 
-			this.figures = function (marginPics) {
+			card.type = ($attrs.card==='')?'':'--'+$attrs.card;
+			$element.addClass('card'+ card.type);
+
+
+			card.figures = function (marginPics) {
 
 				if ( !checkWebpage(componentName) ) { return false; };
 
 	    	$element.find('.card__content').children('div').remove(); //reset
-	    	$element.find('.card__content').css('display', 'flex'); 
+	    	$element.find('.card__content').css('display', 'flex');
 
 	    	var pics = $element.find('img');
 	    	var widthContainer = $element.find('.card__content').width() - marginPics ;
@@ -36,58 +37,50 @@ function card($rootScope, $yemd, checkWebpage){
 	    		});
 
 	    		$element.find('.card__content').append( image )  ;
-	    		
+
 	    	});
 
 	    	$element.find('img').hide();
 	    }
 
-	    this.cover = function() {
+			card.coverAndPhoto = coverAndPhoto;
 
-	    	if ( !checkWebpage(componentName) ) { return false; };
-	    	console.log($element.width());
-	    	$element.find('.card__cover').css({
-	    		height: $element.width() * (9/16),
-	    		backgroundSize: 'cover',
-					'background-image': "url('"+ $element.find('.card__cover__image').attr('src')+"')" 
+
+			function coverAndPhoto () {
+
+				$element.find('.card__cover').css({
+					height: $element.width() * (9/16),
+					backgroundImage: "url('"+ $element.find('.card__cover__image').attr('src')+"')"
 				});
+
+
+				$element.find('.card__photo').css({
+					backgroundImage: "url('"+ $element.find('.card__photo__image').attr('src')+"')"
+				});
+
+				if ( $attrs.card === 'square-picture' ) {
+					$element.css({ backgroundImage: "url('"+ $element.find('.card__cover__image').attr('src')+"')" });
+				}
+
+				$element.find('.card__photo__image').hide();
+
 				$element.find('.card__cover__image').hide();
-	    }
 
-	    this.avatar = function(){
-
-	    	if ( !checkWebpage(componentName) ) { return false; };
-
-	    	$element.find('.card__photo').css({ 
-	    		backgroundSize: 'cover',
-	    		backgroundImage: "url('"+ $element.find('.card__photo__image').attr('src')+"')"  
-	    	});
-
-	    	$element.find('.card__photo__image').hide();
-	    }
+			}
 	    
 		},
 		link: function (scope, element, attrs, cardCtrl ) {
 
-	    if ( attrs.card === 'square-picture' ) {
-	      element.css({
-					'background-image': "url('"+ element.find('.card__cover__image').attr('src')+"')" 
-				});
-	    };
+			if ( !checkWebpage(componentName) ) { return false; };
 
-	    if ( attrs.card === 'figures' ) { cardCtrl.figures(24); };
+			cardCtrl.coverAndPhoto();
 
-	    if ( attrs.card === 'cover' ) { cardCtrl.cover(); };
+	    	if ( attrs.card === 'figures' ) { cardCtrl.figures(24); };
 
-	    if ( attrs.card === 'figure' ) { cardCtrl.cover(); };
-
-	    if ( attrs.card === 'avatar' ) { cardCtrl.avatar(); };
-
-	    $rootScope.$on('resizeWindow', function(e) {
-        if ( attrs.card === 'figures' ) { cardCtrl.figures(8); };
-        if ( attrs.card === 'cover' || attrs.card === 'figure'  ) { cardCtrl.cover(); };
-        if ( attrs.card === 'avatar' ) { cardCtrl.avatar(); };
-      });
+			$rootScope.$on('resizeWindow', function(e) {
+				if ( attrs.card === 'figures' ) { cardCtrl.figures(8); };
+				cardCtrl.coverAndPhoto();
+			});
 						
 	  }
 	}
